425029deb06cb99ed6d70ad7dedcd65b072a7975
Dan Chanivet <chanivetdan@hotmail.com>
2023-08-18 15:10:44 +0000

    Anaconda cloud

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,14 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/8b138bf4-df8a-47b2-b4ca-5157298127b2'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/8b138bf4-df8a-47b2-b4ca-5157298127b2/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'anacondaapps.cloud'
+os.environ['PYTHONANYWHERE_SITE'] = 'nb.anaconda.cloud'
+os.environ['http_proxy'] = 'http://proxy.server:3128'
+os.environ['https_proxy'] = 'http://proxy.server:3128'
+os.environ['no_proxy'] = 'localhost,127.0.0.1,localaddress,.localdomain.com,/var/run/docker.sock'
+os.environ['API_TOKEN'] = '3bb6a5ee34bc22b63f4d34e284a0d0dff31ca89b'
